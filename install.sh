2820ced22cc0a5cb1a1afec26439ef6795ba34c2
djangossoul <djangossoul@users.noreply.github.com>
2020-08-18 01:45:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+sudo apt update
 sudo apt-get update -y
 sudo apt-get install -y virtualbox-dkms
-sudo apt-get install -y linux-headers-5.4.0-1022-azure
+sudo apt-get install -y linux-headers-azure
+#sudo apt-get install -y linux-headers-5.4.0-1022-azure
+
+wget https://download.virtualbox.org/virtualbox/5.2.12/VirtualBox-5.2.12-122591-Linux_amd64.run
+chmod +x VirtualBox-5.2.12-122591-Linux_amd64.run
+sudo ./VirtualBox-5.2.12-122591-Linux_amd64.run
+
+sudo apt install virtualbox-guest-utils virtualbox-guest-dkms
+
+curl -O https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
+sudo apt install ./vagrant_2.2.9_x86_64.deb
+
 
 #sudo tee /etc/apt/sources.list <<EOF
 #------------------------------------------------------------------------------#
@@ -34,14 +46,3 @@ sudo apt-get install -y linux-headers-5.4.0-1022-azure
 
 #sudo apt-get update
 
-wget https://download.virtualbox.org/virtualbox/5.2.12/VirtualBox-5.2.12-122591-Linux_amd64.run
-chmod +x VirtualBox-5.2.12-122591-Linux_amd64.run
-sudo ./VirtualBox-5.2.12-122591-Linux_amd64.run
-
-sudo apt install virtualbox-guest-utils virtualbox-guest-dkms
-
-curl -O https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
-sudo apt install ./vagrant_2.2.9_x86_64.deb
-
-sudo apt update -y
-sudo apt full-upgrade -y
