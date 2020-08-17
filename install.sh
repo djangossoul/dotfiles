ad9d03edb93768472394499c5e98385d78c3d938
djangossoul <djangossoul@users.noreply.github.com>
2020-08-17 23:55:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 sudo apt update
-curl -O https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.deb
-sudo apt install ./vagrant_2.2.6_x86_64.deb
+sudo apt-get update -y
+sudo apt-get install -y virtualbox-dkms
+sudo apt-get install -y linux-headers-azure
 
-echo deb http://ftp.debian.org/debian stretch-backports main contrib > /etc/apt/sources.list.d/stretch-backports.list
-apt update
-apt install virtualbox-guest-dkms virtualbox-guest-x11 linux-headers-$(uname -r)
+wget https://download.virtualbox.org/virtualbox/5.2.12/VirtualBox-5.2.12-122591-Linux_amd64.run
+chmod +x VirtualBox-5.2.12-122591-Linux_amd64.run
+sudo ./VirtualBox-5.2.12-122591-Linux_amd64.run
+
+curl -O https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
+sudo apt install ./vagrant_2.2.9_x86_64.deb
