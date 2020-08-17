3a7a552a3f2e2411aacb9a4d50c56cc41219baf7
djangossoul <djangossoul@users.noreply.github.com>
2020-08-17 23:57:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,5 +9,7 @@ wget https://download.virtualbox.org/virtualbox/5.2.12/VirtualBox-5.2.12-122591-
 chmod +x VirtualBox-5.2.12-122591-Linux_amd64.run
 sudo ./VirtualBox-5.2.12-122591-Linux_amd64.run
 
+sudo apt install virtualbox-guest-utils virtualbox-guest-dkms
+
 curl -O https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
 sudo apt install ./vagrant_2.2.9_x86_64.deb
