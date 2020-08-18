544b3880f34b50f7fdcb3df5eed2d83cf1424073
djangossoul <djangossoul@users.noreply.github.com>
2020-08-18 14:33:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt-get update -y
-sudo apt-get install -y virtualbox-dkms
-sudo apt-get install -y linux-headers-azure
+apt update
 
-wget https://download.virtualbox.org/virtualbox/5.2.12/VirtualBox-5.2.12-122591-Linux_amd64.run
-chmod +x VirtualBox-5.2.12-122591-Linux_amd64.run
-sudo ./VirtualBox-5.2.12-122591-Linux_amd64.run
+# Install VirtualBox
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc -O- | apt-key add -
+sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian xenial contrib" >> /etc/apt/sources.list.d/virtualbox.list'
+apt-get update
+apt-get install -y virtualbox-5.2 || /bin/true
+apt-get install -y -f
 
-sudo apt install virtualbox-guest-utils virtualbox-guest-dkms
+# Install Virtualbox Extension Pack
+VBOX_VERSION=`dpkg -s virtualbox-5.2 | grep '^Version: ' | sed -e 's/Version: \([0-9\.]*\)\-.*/\1/'` ; \
+    wget http://download.virtualbox.org/virtualbox/${VBOX_VERSION}/Oracle_VM_VirtualBox_Extension_Pack-${VBOX_VERSION}.vbox-extpack ; \
+    VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-${VBOX_VERSION}.vbox-extpack ; \
+    rm Oracle_VM_VirtualBox_Extension_Pack-${VBOX_VERSION}.vbox-extpack
 
-curl -O https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
-sudo apt install ./vagrant_2.2.9_x86_64.deb
+# Install Vagrant
+wget https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.deb
+dpkg -i vagrant_2.0.1_x86_64.deb
